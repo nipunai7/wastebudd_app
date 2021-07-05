26911d6f3505c7444db628f2ebb7c0217d065f64
Nipuna Muansinghe <mnipunai7@gmail.com>
2021-07-06 00:55:39 +0530

    6/7/2021

--- lib/Models/user.dart
+++ lib/Models/user.dart
@@ -3,12 +3,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class User{
+class Userbudd{
 
   static SharedPreferences sharedPreferences;
-  static FirebaseUser user;
   static FirebaseAuth auth;
-  static Firestore firestore ;
 
   static final String userName = 'name';
   static final String jdate = 'jdate';
